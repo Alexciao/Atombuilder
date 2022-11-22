e131e4a8a968efaa86c201900fb5c755da16d2d1
Alexciao <alex.corazza@icloud.com>
2022-11-22 20:15:29 +0100

    Add Splash Text backend

--- Assets/Scripts/JSON/JSONManager.cs
+++ Assets/Scripts/JSON/JSONManager.cs
@@ -9,23 +9,31 @@ using UnityEngine;
 public class JSONManager : MonoBehaviour
 {
     public PeriodicTable table;
-    
+    public Splashes _splashes;
+
+    public Element _rElement;
+    public string _rSplash;
+
     [Header("References")]
-    [SerializeField] private string _jsonUrl;
     [SerializeField] private TextMeshProUGUI _elementName;
-    [ReadOnly] public Element _rElement;
     [SerializeField] private GuessManager _guessManager;
     [Header("Settings")]
     [SerializeField] private bool showElement;
     [SerializeField] private bool downloadFromUrl;
+    [Header("URLs")]
+    [SerializeField] private string _tableUrl;
+    [SerializeField] private string _splashesUrl;
     
     void Start()
     {
-        table = LoadJson(_jsonUrl);
+        table = LoadTable(_tableUrl);
+        
         RerollElement();
         if (showElement) _elementName.text = _rElement.symbol;
     }
 
+    #region Periodic Table
+    
     public Element ChooseRandomElement(PeriodicTable table)
     {
         int elements = table.elements.Count;
@@ -33,15 +41,14 @@ public class JSONManager : MonoBehaviour
         Debug.Log("Random element: " + table.elements[randomElement].symbol);
         return table.elements[randomElement];
     }
-    
     public void RerollElement()
     {
         _rElement = ChooseRandomElement(table);
         if (showElement) _elementName.text = _rElement.symbol;
         if (_guessManager != null) _guessManager.SetAtom();
     }
-    
-    PeriodicTable LoadJson(string jsonDir)
+
+    PeriodicTable LoadTable(string jsonDir)
     {
         if (!downloadFromUrl)
         {
@@ -57,4 +64,38 @@ public class JSONManager : MonoBehaviour
         }
     }
 
+    #endregion
+
+    #region Splash Text
+
+    public string ChooseRandomSplash(Splashes splashes)
+    {
+        int splashesCount = splashes.splashes.Count;
+        int randomSplash = Random.Range(0, splashesCount);
+        Debug.Log("Random splash: " + splashes.splashes[randomSplash]);
+        return splashes.splashes[randomSplash];    
+    }
+    
+    public void RerollSplash()
+    {
+        _rSplash = ChooseRandomSplash(_splashes);
+    }
+    
+    Splashes LoadSplashes(string jsonDir)
+    {
+        if (!downloadFromUrl)
+        {
+            string json = File.ReadAllText(jsonDir);
+            Splashes data = JsonConvert.DeserializeObject<Splashes>(json);
+            return data;
+        }
+        else
+        {
+            string json = new WebClient().DownloadString(jsonDir);
+            Splashes data = JsonConvert.DeserializeObject<Splashes>(json);
+            return data;
+        }
+    }
+    
+    #endregion
 }
---
+++ Assets/Scripts/JSON/SplashTexts.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+public class Splashes
+{ 
+    public List<string> splashes { get; set; }
+}
+
